3a8bfdfee28e80866b9d734ada54fb8a7b0978c7
Rgameroco <39881338+Rgameroco@users.noreply.github.com>
2021-09-28 00:35:48 -0500

    github

---
+++ worker/urls.py
@@ -0,0 +1,31 @@
+from django.urls import path, include
+from django.contrib.auth.models import User
+from rest_framework import serializers, viewsets, routers
+from graphene_django.views import GraphQLView
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+# Routers provide a way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('v1/', include('fieldWorker.urls')),
+    path("graphql", GraphQLView.as_view(graphiql=True)),
+]
